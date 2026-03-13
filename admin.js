22530bd50948cee989e14a69ebb7a3f7651ba68d
briannewton574-ai <briannewton574@gmail.com>
2026-03-13 17:59:05 +0300

    Delete admin.js

--- admin.js
+++
@@ -1,7 +0,0 @@
-<script>
-// Check if user is admin
-if(localStorage.getItem("isAdmin") !== "true"){
-    alert("Access denied! Please log in first.");
-    window.location.href = "login.html"; // redirect to login page
-}
-</script>
